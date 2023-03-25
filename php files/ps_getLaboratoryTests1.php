58180cb22971bda8f4fb40a1d032ca4867a2e6d2
Preethi S <112772746+IamPreethi-S@users.noreply.github.com>
2023-03-25 14:31:25 -0500

    Added php files for update tests and to fetch all tests

--- php files/ps_getLaboratoryTests1.php
+++ php files/ps_getLaboratoryTests1.php
@@ -5,7 +5,7 @@ $laboratoryid = $_POST["laboratoryid"];
 
 header("Content-Type: application/json");
 
-$sql_query = "select LT.testname,P.firstname,P.lastname,LT.testreport,LT.tdate,LT.status FROM labtests LT JOIN visits V on LT.visitid = V.visitid JOIN patient P ON P.patientid=V.patientid WHERE laboratoryid = $laboratoryid;";
+$sql_query = "select LT.testname,P.firstname,P.lastname,LT.testreport,LT.laboratoryid,LT.visitid FROM labtests LT JOIN visits V on LT.visitid = V.visitid JOIN patient P ON P.patientid=V.patientid WHERE laboratoryid = $laboratoryid and LT.status ='Pending';";
 
 $results = mysqli_query($conn, $sql_query);
 
@@ -15,8 +15,8 @@ while ($data = mysqli_fetch_assoc($results)) {$test = ["testname" => $data["test
 			"firstname" => $data["firstname"],
 			"lastname" => $data["lastname"],
 			"testreport" => $data["testreport"],
-			"tdate" => $data ["tdate"],
-			"status" => $data ["status"]];
+			"laboratoryid" => $data["laboratoryid"],
+			"visitid" => $data["visitid"]];
 array_push($testsList, $test);
 }
 echo json_encode($testsList);
